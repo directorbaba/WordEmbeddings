0f207c7fde9f340d9aa942e2762ec4a042bb410e
Govind Kumar Arora <48126302+directorbaba@users.noreply.github.com>
2019-03-18 21:38:04 +0530

    Uploading the files

---
+++ loadmodel.py
@@ -0,0 +1,11 @@
+import fasttext
+
+classifier = fasttext.supervised('Train.csv', 'model', label_prefix='label_', lr=1.0, dim=100, epoch=25)
+
+result= classifier.test('Test.csv')
+
+print(result.precision)
+print(result.recall)
+print(result.nexamples)
+
+
